c706528ba6b018abc5e0a14b3ebc48c49c216e73
adhin <adhin.adish@excelenciaconsulting.com>
2025-08-09 22:22:40 +0530

    Added the models

---
+++ Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementApi.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; } 
+
+        public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
